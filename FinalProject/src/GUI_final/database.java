902f20485e6233788d09d0ff85b26f257fbbf0e5
Esha Tanveer <eshatanveer7@gmail.com>
2021-06-05 16:55:39 -0700

    ..

--- FinalProject/src/GUI_final/database.java
+++ FinalProject/src/GUI_final/database.java
@@ -15,10 +15,10 @@ public class database {
     try{
       Class.forName("com.mysql.cj.jdbc.Driver"); 
        Connection con=DriverManager.getConnection(  
-        "jdbc:mysql://localhost:3306/NewQuiz","root","Es_20022002 kh");  
+        "jdbc:mysql://localhost:3306/quiz_data","root","Es_20022002 kh");  
         //here sonoo is database name, root is username and password  
         Statement stmt=con.createStatement();  
-        ResultSet rs=stmt.executeQuery("select * from ");  
+        ResultSet rs=stmt.executeQuery("select * from user");  
         while(rs.next())  
         System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
         con.close();  
